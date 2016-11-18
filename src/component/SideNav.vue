e568ccb1c30b1f4dd45716555aaec7f9e074609c
钟乘永 <zhongcy93@163.com>
2016-11-18 08:51:02 +0800

    试用vscode

--- src/component/SideNav.vue
+++ src/component/SideNav.vue
@@ -2,7 +2,7 @@
     <div>
         <el-row class="tac">
             <el-col :span="6">
-                <el-menu default-active="2" class="el-menu-vertical-demo">
+                <el-menu default-active="1" class="el-menu-vertical-demo">
                     <div class="secondTitle">
                         标签<span class="distance"><el-button type="primary" icon="plus">添加</el-button></span>
                     </div>
@@ -12,10 +12,10 @@
                             <el-menu-item>{{index}}:{{tag.name}}</el-menu-item>
                         </li>
                     </ul>-->
-                    <el-menu-item v-for="(tag,index) in tags">{{tag.name}}</el-menu-item>
-                    <el-menu-item index="1">导航一</el-menu-item>
+                    <el-menu-item v-for="(tag,index) in tags" :index=String(index)>{{tag.name}}</el-menu-item>
+<!--                    <el-menu-item index="1">导航一</el-menu-item>
                     <el-menu-item index="2">导航二</el-menu-item>
-                    <el-menu-item index="3">导航三</el-menu-item>
+                    <el-menu-item index="3">导航三</el-menu-item>-->
                 </el-menu>
             </el-col>
         </el-row>

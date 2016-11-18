1e9621e7bb527ae10e329506f07b30dac0c340d5
钟乘永 <zhongcy93@163.com>
2016-11-18 09:41:57 +0800

    页面功能

--- src/component/SideNav.vue
+++ src/component/SideNav.vue
@@ -4,14 +4,16 @@
             <el-col :span="6">
                 <el-menu default-active="1" class="el-menu-vertical-demo">
                     <div class="secondTitle">
-                        标签<span class="distance"><el-button type="primary" icon="plus">添加</el-button></span>
+                        标签<span class="distance"><el-button type="primary" icon="plus" @click='add'>添加</el-button></span>
+                        <!-- 这样也是可以的，属性中使用@click
+                            <el-button v-for="i in 2" @click='test'>添加</el-button></span>-->
                     </div>
-
 <!--                    <ul>
                         <li v-for="(tag,index) in tags">
                             <el-menu-item>{{index}}:{{tag.name}}</el-menu-item>
                         </li>
                     </ul>-->
+                    <!--使用:index来标识属性？ -->
                     <el-menu-item v-for="(tag,index) in tags" :index=String(index)>{{tag.name}}</el-menu-item>
 <!--                    <el-menu-item index="1">导航一</el-menu-item>
                     <el-menu-item index="2">导航二</el-menu-item>
@@ -37,7 +39,12 @@
         data(){
             return {
                 msg: 'hello vue',
-                tags: [{name: '导航一'}, {name: '导航二'}]
+                tags: [{name: '工作'}, {name: '运动'}]
+            }
+        },
+        methods:{
+            add(){
+                this.tags.push({name: '学习'})
             }
         }
     }

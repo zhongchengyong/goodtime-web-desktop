c6b943c78820649d4b5e703943ae0147667a2cc9
钟乘永 <zhongcy93@163.com>
2016-11-23 23:06:01 +0800

    代码提交1123-3

--- src/component/Note.vue
+++ src/component/Note.vue
@@ -21,6 +21,9 @@
                             <el-input type="textarea" v-model="form.content" placeholder="请输入任务内容">
                             </el-input>
                         </el-form-item>
+                        <el-form-item label="任务分数" :label-width="formLabelWidth">
+                        <el-input-number v-model="form.taskScore" :min="1" :max="10"></el-input-number>
+                        </el-form-item>
                     </el-form>
                     <div slot="footer" class="dialog-footer">
                         <el-button @click.native="dialogFormVisible = false">取 消</el-button>
@@ -51,7 +54,9 @@
                 form:{
                     name:'',
                     content:'',
-                }
+                    taskScore:1,
+                },
+                formLabelWidth: '120px'
             }
         },
         methods: {
@@ -63,12 +68,15 @@
             },
             addTask(){
                 let vm = this;
-                vm.$http.post(vm.taskUrl,{taskName:vm.form.name,taskContent:vm.form.content}).then((response)=>{
+                vm.$http.post(vm.taskUrl,{taskName:vm.form.name,taskContent:vm.form.content,taskScore:vm.form.taskScore}).then((response)=>{
                     if(response.body.errorCode==0){
+                        vm.form={};
                         vm.$message({
                             message: '添加任务成功',
                             type: 'success',
                         });
+                        vm.dialogFormVisible = false;
+                        vm.query();
                     }else{
                         vm.$message({
                             message: response.body.errorMsg,

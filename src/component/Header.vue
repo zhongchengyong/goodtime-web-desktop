b5d479e2ccfa88ac736bcc842d477f45c5968b30
钟乘永 <zhongcy93@163.com>
2016-12-08 10:21:46 +0800

    commit

--- src/component/Header.vue
+++ src/component/Header.vue
@@ -7,10 +7,22 @@
         background-color: #ffffff;
         height: 60px;
     }
-    .message,.logout{
+    .logout{
         position: absolute;
         left: 50%;
         height: 60px;
+    }
+    .sup-message{
+        transform: translateY(-30%) translateX(60%);
+        background-color: #ff4949;
+        border-radius: 10px;
+        color: #fff;
+        font-size: 12px;
+        white-space: nowrap;
+        height: 18px;
+        line-height: 18px;
+        padding: 0 6px;
+        text-align: center;
     }
 </style>
 <template>
@@ -25,14 +37,10 @@
             </el-submenu>
             <el-menu-item index="diary">日志管理</el-menu-item>
             <el-menu-item index="task">我的任务</el-menu-item>
-            <!--<el-badge value="new" class="item">
-                <el-button size="small">消息</el-button>
-            </el-badge>-->
-            <div class="el-badge item">
-                <button type="button" class="el-button el-button--default el-button--small">
-                    <span>评论</span></button>
-                    <sup class="el-badge__content is-fixed">12</sup>
-            </div>
+            <a class="el-menu-item" @click.prevent="getMessage" title="消息">
+                消息
+                <sup class="sup-message" v-show="newMsg">new</sup>
+            </a>
             <a class=" logout el-menu-item" @click.prevent="logout" title="退出">退出</a>
         </el-menu>
         <div class="line"></div>
@@ -42,6 +50,7 @@
     export default{
         data(){
             return{
+                newMsg:true,
             }
         },
 
@@ -52,6 +61,12 @@
                     if(response.body.errorCode==0){
                         window.location.href = '/login';
                     }
+                })
+            },
+            getMessage(){
+                let vm = this;
+                vm.$http.get('goodtime/syssup-Message').then((response)=>{
+                    console.log(response.body.data);
                 })
             }
         }
